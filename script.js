57306fa82d619cd628396914e6a0f12148a7d0cd
Jerry Hara <jerrykeiko98@gmail.com>
2023-12-07 21:58:09 -0800

    Added logic to assign equal height and width to each square to fit inside container.

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const width = 960;
 const height = 960;
 const container = document.querySelector(".container")
 
-// Create 16 square divs
-for (let i = 0; i < 32; i++) {
+// Create 32 square divs
+for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square")
+    square.textContent = i
     container.appendChild(square)
 }
+
+// Style each div to take up an equal amount of space in container
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+    let length = height / 16;
+    length = length.toString() + "px";
+    square.style.height = length;
+    square.style.width = length;
+})
