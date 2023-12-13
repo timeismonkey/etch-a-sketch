f0e0276b7908207a9dd8846bd2aaecfd47a1a753
Jerry Hara <jerrykeiko98@gmail.com>
2023-12-13 14:16:52 -0800

    Added square count button logic. Added logic for makeSquares(), a function that takes as input the amount of squares per side in container. Also set default container to 16x16.

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const container = document.querySelector(".container")
 const width = container.offsetWidth;
 const height = container.offsetHeight;
+const squareCountButton = document.querySelector(".square-count");
 
-// Create 32 square divs
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square")
-    container.appendChild(square)
-}
 
 // Style each div to take up an equal amount of space in container
-const squares = document.querySelectorAll(".square");
-squares.forEach(square => {
-    let length = height / 16;
-    length = length.toString() + "px";
-    square.style.height = length;
-    square.style.width = length;
-})
+function makeSquares(squareCount) {
+    container.innerHTML = "";
+    let totalSquares = squareCount ** 2;
+    // Create totalSquares divs
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square")
+        // square.textContent = i
+        container.appendChild(square)
+    }
+
+    // Style the width and height of squares
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        let length = height / squareCount;
+        length = length.toString() + "px";
+        square.style.height = length;
+        square.style.width = length;
+    })
+}
+
+// Default to 16x16 gird
+makeSquares(16);
+
+// Add event listener to square count button to prompt user for square count per side
+squareCountButton.addEventListener("click", () => {
+    let squareCount;
+    do {
+        squareCount = prompt("How many squares per side?")
+    } while (parseInt(squareCount) > 100 || parseInt(squareCount) < 0);
+    makeSquares(parseInt(squareCount));
+});
